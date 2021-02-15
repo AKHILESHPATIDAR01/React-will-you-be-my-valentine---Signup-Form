75c3d8a465b73922ad82a62c9dd2932cce4b60aa
AKHILESHPATIDAR01 <71465599+AKHILESHPATIDAR01@users.noreply.github.com>
2021-02-15 11:53:08 +0530

    Update index.js
    ss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+// import 'bootstrap';
+// import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
