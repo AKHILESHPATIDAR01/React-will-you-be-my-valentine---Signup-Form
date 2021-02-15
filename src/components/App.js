91302ba99a2da1123ae48650d88e4fcfba723bb4
AKHILESHPATIDAR01 <71465599+AKHILESHPATIDAR01@users.noreply.github.com>
2021-02-15 11:52:33 +0530

    Update App.js
    ff

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,150 @@
 import React, {Component, useState} from "react";
+import { Button, Container, Form } from "react-bootstrap";
 import '../styles/App.css';
 
 const App = () => {
+
+  const [errorName , setErrorName] = React.useState("");
+  const [errorEmail , setErrorEmail] = React.useState("");
+  const [errorGender , setErrorGender] = React.useState("");
+  const [errorPassword , setErrorPassword] = React.useState("");
+  const [errorPhoneNo , setErrorPhoneNo] = React.useState("");
+  const [errorMsg , setErrorMsg] = React.useState("");
+
+  const [name , setName] = React.useState("");
+  const [email , setEmail] = React.useState("");
+  const [gender , setGender] = React.useState("");
+  const [password , setPassword] = React.useState("");
+  const [phoneNo , setPhoneNo] = React.useState("");
+
+  const handleInputChangeName =(event)=>{
+    setName(event.target.value);
+  }
+  const handleInputChangeEmail =(event)=>{
+    setEmail(event.target.value);
+  }
+  const handleInputChangeGender =(event)=>{
+    setGender(event.target.value);
+    
+  }
+  const handleInputChangePassword =(event)=>{
+    setPassword(event.target.value);
+  }
+  const handleInputChangePhoneNo =(event)=>{
+    setPhoneNo(event.target.value);
+  }
+
+
+  const handleClick = () =>{
+    console.log("My Gender is : ",gender);
+    // name
+    var lettersName = /^[A-Za-z]+$/;
+    // var lettersEmail = /\S+@\S+/;
+    var lettersNumber = /^[0-9\b]+$/;
+
+    if(!name.match(lettersName)){
+        // alert('Your name have accepted : you can try another');
+        setErrorName("Only alphanumeric allowed");
+        return true;
+    }
+    else{
+        setErrorName("");
+    }
+
+    // email
+    if(!email.includes('@')){
+      // alert('INVALID');
+      setErrorEmail("Email must contain @");
+    }
+    else{
+      setErrorEmail("");
+    }
+
+    // phonep No
+
+    if(!phoneNo.match(lettersNumber) || phoneNo.length==10){
+      // alert('INVALID');
+      setErrorPhoneNo("Phone Number must contain only numbers");
+    }
+    else{
+      setErrorPhoneNo("");
+    }
+
+    // password
+    if(password.length <=6){
+      setErrorPassword("Password must contain atleast 6 letters");
+    }
+    else{
+      setErrorPassword("");
+    }
+
+
+    // gender
+    if(gender==""){
+      setErrorGender("Please identify as male, female or others");
+    }
+    else{
+      setErrorGender("");
+    }
+
+    // else
+    // {
+    //     alert('Please input alphabet characters only');
+    //     return false;
+    // }
+
+
+  }
+
   return (
-    <div id="main"></div>
+    <div id="main">
+
+        {/* <Button className="success">Success</Button> */}
+
+        <Container>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control value={name} data-testid = 'name' type="text" onChange={handleInputChangeName} placeholder="Enter Your Name" />
+            <p className="text-danger">{errorName}</p>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control value={email} data-testid = 'email' type="text" onChange={handleInputChangeEmail} placeholder="Enter Your Email address" />
+            <p className="text-danger">{errorEmail}</p>
+          </Form.Group>
+
+          <Form.Group controlId="formGridState">
+            <Form.Label>Gender</Form.Label>
+            <Form.Control value={gender} data-testid = 'gender' as="select" onChange={handleInputChangeGender} defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>Male</option>
+              <option>Female</option>
+              <option>Other</option>
+            </Form.Control>
+            <p className="text-danger">{errorGender}</p>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control value={phoneNo} data-testid = 'phoneNumber' type="text" onChange={handleInputChangePhoneNo} placeholder="Enter Your Phone Number" />
+            <p className="text-danger">{errorPhoneNo}</p>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Password</Form.Label>
+            <Form.Control value={password} data-testid = 'password' type="password" onChange={handleInputChangePassword} placeholder="Enter Password" />
+            <p className="text-danger">{errorPassword}</p>
+          </Form.Group>
+
+           <Button data-testid = 'submit' variant="primary" onClick={handleClick}>
+            Submit
+          </Button>
+
+        </Form>
+        </Container>
+    </div>
   )
 }
 
